afa964cb162cee73f295df273feacfdc51ecb8db
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2022-04-21 10:14:03 -0700

    ENH: update displays

--- somsScreens/launch_mr1k4_ui.sh
+++ somsScreens/launch_mr1k4_ui.sh
@@ -2,4 +2,4 @@
 
 source /cds/group/pcds/pyps/conda/pcds_conda
 
-`pydm --hide-nav-bar --hide-menu-bar -m '{"DISP_NAME":"MR1K4", "BASE_PV":"MR1K4:SOMS"}' /reg/g/pcds/epics/ioc/xrt/HOMS_XRT/latest/homsScreens/mirrorScreen.py &`
+`pydm --hide-nav-bar --hide-menu-bar -m '{"DISP_NAME":"MR1K4", "BASE_PV":"MR1K4:SOMS"}' /reg/g/pcds/epics/ioc/tmo/lcls-plc-tmo-optics/latest/somsScreens/mirrorScreen.py &`
